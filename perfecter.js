a37e3d689180853e13cc7c53c4e32664c2e0b7f7
Oleg Frolov <frolololeg@gmail.com>
2016-11-25 19:15:59 +0300

    Added other ratio

--- perfecter.js
+++ perfecter.js
@@ -1,31 +1,37 @@
 var doc = context.document,
 	selection = context.selection,
 	layer = selection.firstObject(),
-    fontSizeSelectedTextLayer = 0,
-    goldenRatio = 1.618,
-    sketch = context.api();
+	fontSizeSelectedTextLayer = 0,
+  sketch = context.api(),
+
+	// Proportions
+	goldenRatio = 1.618,
+	silverRatio = 2.214,
+	squareOf2 = 1.414,
+	squareOf3 = 1.732,
+	squareOf5 = 2.236,
+	pi = 3.142;
 
 // Detect MSTextLayer
-
 if (layer.className() == "MSTextLayer") {
-    setGoldenRationLineHieghtForSelectedTextLayer()
+	setGoldenRationLineHeightForSelectedTextLayer();
 } else {
-	doc.showMessage("You should select one text layer only")
+	doc.showMessage("You should select one text layer only");
 }
 
 // Set GR lineHeight for selected testLayer
-function setGoldenRationLineHieghtForSelectedTextLayer () {
+function setGoldenRationLineHeightForSelectedTextLayer () {
 		fontSizeSelectedTextLayer = layer.fontSize()
-        var fontSizeSelectedTextLayerFromAlert = sketch.getStringFromUser("Test", fontSizeSelectedTextLayer);
-        layer.setLineHeight(fontSizeSelectedTextLayerFromAlert * goldenRatio)
-
+    var getChoosedRationFromUSer = sketch.getSelectionFromUser("Choose ration for lineHeight", [20, 40], 1);
+		log(parseInt(getChoosedRationFromUSer))
+    layer.setLineHeight(fontSizeSelectedTextLayer * getChoosedRationFromUSer)
 }
 
-log(layer.className())
-log(layer.name())
+// log(layer.className())
+// log(layer.name())
 
-log(layer.fontSize())
-log(layer.lineHeight())
+// log(layer.fontSize())
+// log(layer.lineHeight())
 
 // layer.setFontSize(10)
 // layer.setLineHeight(100)
